19e80e93b3e2003fd341d4515d646b554a2e7350
babul101 <babuldeb101@gmail.com>
2024-02-11 11:22:45 +0530

    created javascript file to define a basic set up

--- main.js
+++ main.js
@@ -1,2 +1,79 @@
 import './style.css'
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import { TextPlugin } from 'gsap/TextPlugin'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import SplitType from 'split-type';
+import * as dat from 'dat.gui'
+import * as THREE from 'three';
+import Lenis from '@studio-freight/lenis'
 
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(ScrollTrigger);
+
+
+function initThreeJS(){
+    // Debug
+  const gui = new dat.GUI()
+  dat.GUI.toggleHide()
+
+  // Canvas
+  const canvas = document.querySelector('canvas.webgl')
+
+  // Scene
+  scene = new THREE.Scene()
+
+  // Middle stuff
+
+
+  /**
+   * Sizes
+   */
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+   * Camera
+   */
+  // Base camera
+  camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+  camera.position.x = 0
+  camera.position.y = 0
+  camera.position.z = 2
+  scene.add(camera)
+  /**
+   * Renderer
+   */
+  renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+}
+  
+
+
+document.addEventListener('DOMContentLoaded',() => {
+    initThreeJS();
+})
