507ad74f0b430fee1c1c30d221a4f8cfc39a2716
babul101 <babuldeb101@gmail.com>
2024-02-16 13:47:40 +0530

    updated the details component

--- main.js
+++ main.js
@@ -137,6 +137,38 @@ function animateWords() {
   setInterval(updateText, 3000);
 }
 
+function inspectionSection(){
+    const inspectionTl = gsap.timeline({ 
+        scrollTrigger: {
+          trigger: '.inspection',
+          start: 'top bottom',
+          end: 'bottom top',
+          scrub: true
+        }
+      })
+
+      inspectionTl
+    .to('.inspection h2', {
+      y: -100
+    })
+    .to('.ring-bg', {
+        y: -50,
+        height: 300
+      }, "<")
+
+      gsap.to('.marquee h3', {
+
+        scrollTrigger: {
+          trigger: '.marquee h3',
+          start: 'top 80%',
+          end: 'bottom top',
+          scrub: true
+        },
+        x: 200
+      })
+    
+}
+
 function initRenderLoop() {
   const clock = new THREE.Clock();
 
@@ -182,5 +214,6 @@ document.addEventListener("DOMContentLoaded", () => {
   initThreeJS();
   initRenderLoop();
   animateWords();
+  inspectionSection();
   setupSmoothScroll();
 });
