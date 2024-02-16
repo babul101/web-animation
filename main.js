c737df8eaad5c25821057f994f4c4e6d5a67c72b
babul101 <babuldeb101@gmail.com>
2024-02-16 13:57:25 +0530

    Putting the horizontal slider effect to the images

--- main.js
+++ main.js
@@ -169,6 +169,55 @@ function inspectionSection(){
     
 }
 
+function sliderSection(){
+    let mm = gsap.matchMedia()
+
+  mm.add("(min-width: 768px)", () => {
+
+    let slider = document.querySelector('.slider')
+    let sliderSections = gsap.utils.toArray('.slide')
+
+    let sliderTl = gsap.timeline({
+      defaults: {
+        ease: 'none'
+      },
+      scrollTrigger: {
+        trigger: slider,
+        pin: true,
+        scrub: 1,
+        end: () => "+=" + slider.offsetWidth
+      }
+    })
+
+    sliderTl
+      .to(slider, {
+        xPercent: -66
+      }, "<")
+      .to('.progress', {
+        width: '100%'
+      }, "<")
+
+    sliderSections.forEach((stop, index) => {
+      const slideText = new SplitType(stop.querySelector('.slide-p'), { types: 'chars' });
+
+      sliderTl.from(slideText.chars, {
+        opacity: 0,
+        y: 10,
+        stagger: .03,
+        scrollTrigger: {
+          trigger: stop.querySelector('.slide-p'),
+          start: 'top bottom',
+          end:   'bottom center',
+          containerAnimation: sliderTl,
+          scrub: true,
+        }
+      })
+    })
+
+  })
+
+}
+
 function initRenderLoop() {
   const clock = new THREE.Clock();
 
@@ -215,5 +264,6 @@ document.addEventListener("DOMContentLoaded", () => {
   initRenderLoop();
   animateWords();
   inspectionSection();
+  sliderSection();
   setupSmoothScroll();
 });
